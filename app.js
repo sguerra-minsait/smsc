73fb0f98f6714f637fa4f961d230dffff3e50107
DimitarNikolaevVasilev <49063656+DimitarNikolaevVasilev@users.noreply.github.com>
2019-05-14 16:23:58 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 console.log("Loading...");
 
-require('./mod/check_env')();
+//require('./mod/check_env')();
 
 const security = require('./mod/security');
+const post = require('./mod/post_request');
 var port = process.env.PORT || 3000;
 
 var app = require('./mod/server');
@@ -16,6 +17,27 @@ app.post('/execute', security.check_token, (req,res) => {
 	var datos = req.body.inArguments[0];
 	console.log(datos);
 });
+
+
+app.post('/short', (req, res) => {
+	var long_url = req.body.long_url;
+	console.log(long_url);
+	post({
+		url: 'https://api-ssl.bitly.com/v4/shorten',
+		body: {long_url: long_url},
+		headers: {
+			'Authorization': 'Bearer f2fe6fde72541aee06d8c4093487c941b24fa87b',
+			'Content-Type': 'application/json'
+		}
+	}).then((body, response) => {
+		var data = JSON.parse(body);
+		res.end(JSON.stringify({link: data.link}));
+	}).catch(err => {
+		console.log(err);
+		res.end(JSON.stringify({error: err.message}));
+	});
+});
+
 
 
 
