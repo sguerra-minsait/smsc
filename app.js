3a7d8ccccbde99e4bf6071062af50c99641df5f0
DimitarNikolaevVasilev <49063656+DimitarNikolaevVasilev@users.noreply.github.com>
2019-05-24 09:18:23 +0200

    Update app.js

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ app.post('/execute', security.check_token, (req,res) => {
 	console.log('EXECTURE BODY: ', req.rawBody, req.body);
 	var datos = req.body.inArguments[0];
 	console.log(datos);
+	res.status(Math.random () % 2 == 0 ? 200 : 400).end();
 });
 
 
